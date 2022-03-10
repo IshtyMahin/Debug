534e0ee3a75894b075480294e6d046f6e6e77e47
Ishtiaq uddin <ishty119@gmail.com>
2022-03-10 21:27:44 +0600

    display report

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post);
+  // console.log(post);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -154,7 +154,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    // console.log(reportedPosts);
+      reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
